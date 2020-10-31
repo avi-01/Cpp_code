9b0d05e6565d3ce3cf2a58ad4d98e0d1db00f685
avi-01 <avnishmay@gmail.com>
2020-11-01 00:38:23 +0530

    Added button to solution file

---
+++ usetemplate.cpp
@@ -0,0 +1,43 @@
+/*
+    * @authr AVINISH KUMAR
+    * @college BIT MESRA
+*/
+ 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void solve() {
+    
+}
+
+
+int main()
+{
+ 
+    #ifdef L
+        local = 'L';
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
+    #endif
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+ 
+    ll t1;
+    
+    t1 = 1;
+ 
+ 
+    cin>>t1;
+ 
+    cal();
+ 
+    while (t1--) {
+        solve();
+    }
+ 
+    
+}

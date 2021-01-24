128dac28458c4947610cb6d7eb65406e0d10c317
avi-01 <avnishmay@gmail.com>
2021-01-24 23:09:26 +0530

    Update

--- newTemplate.cpp
+++ newTemplate.cpp
@@ -2,15 +2,14 @@
     * @authr AVINISH KUMAR
     * @college BIT MESRA
 */
-
+ 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
+#include <ext/pb_ds/assoc_container.hpp> 
 using namespace std;
-
+using namespace __gnu_pbds;
+ 
 #define MOD 1000000007
-#define maxn 100000000000017
+#define maxn 100000000000017    
 #define endl "\n"
 #define mk make_pair
 #define pll pair<ll, ll>
@@ -20,36 +19,37 @@ using namespace std;
 #define ff first
 #define ss second
 #define pb push_back
-
-
+ 
+typedef long long ll;
+typedef long double ld;
+ 
+ 
 typedef tree<ll, null_type, less<ll>, rb_tree_tag,
              tree_order_statistics_node_update>
     ordered_set;
-    
+ 
+ 
 #define o(...) if(local=='L') {__f(#__VA_ARGS__, __VA_ARGS__); cout<<endl;}
-
+ 
 #define Ns 1000007
-
-typedef long long ll;
-typedef long double ld;
-
+ 
 char local = 'O';
-
+ 
 ////////////////#####################INPUT###############///////////////////////
-
+ 
 template <typename T>
 void read(T &p)
 {
     cin >> p;
 }
-
+ 
 template <typename T, typename T1>
 void read(pair<T, T1> &p)
 {
     read(p.ff);
     read(p.ss);
 }
-
+ 
 template <typename T>
 void read(T arr[], ll n)
 {
@@ -58,7 +58,7 @@ void read(T arr[], ll n)
         read(arr[i]);
     }
 }
-
+ 
 template <typename T>
 void read(vector<T> &arr)
 {
@@ -67,16 +67,16 @@ void read(vector<T> &arr)
         read(arr[i]);
     }
 }
-
-
+ 
+ 
 ////////////////#####################OUPUT###############///////////////////////
-
+ 
 template <typename T>
 void out(T& n)
 {
     cout << n;
 }
-
+ 
 template <typename T, typename T1>
 void out(pair<T, T1> &p)
 {
@@ -86,7 +86,7 @@ void out(pair<T, T1> &p)
     out(p.ss);
     cout << ")";
 }
-
+ 
 template <typename T>
 void out(T arr[], ll n)
 {
@@ -96,25 +96,62 @@ void out(T arr[], ll n)
         cout << " ";
     }
 }
-
+ 
 template <typename T>
 void out(vector<T> &arr)
 {
+ 
     cout << "[ ";
     for (ll i = 0; i < arr.size(); i++)
     {
         out(arr[i]);
-        if(i!=arr.size()-1)
-        cout << ", ";
+        if (i!=arr.size()-1)
+            cout << ", ";
     }
     cout << " ]";
+ 
+}
+ 
+ 
+ 
+template <typename T>
+void out(set<T> &arr)
+{
+    for (auto i:arr)
+    {
+        out(i);
+        cout << " ";
+    }
 }
 
+template <typename T>
+void out(multiset<T> &arr)
+{
+    for (auto i:arr)
+    {
+        out(i);
+        cout << " ";
+    }
+}
+ 
+template <typename T, typename T1>
+void out(map<T,T1> &m)
+{
+    for (auto i:m)
+    {
+        out(i.first);
+        cout<<" -> ";
+        out(i.second);
+        cout<<", ";
+    }
+}
+ 
 template <typename Arg1>
-void __f(const char* name, Arg1&& arg1) { cout << name << " : ";
+void __f(const char* name, Arg1&& arg1) {
+    cout << name << " : ";
     out(arg1);
 }
-
+ 
 template <typename Arg1, typename... Args>
 void __f(const char* names, Arg1&& arg1, Args&&... args) {
     const char *comma = strchr(names + 1, ',');
@@ -123,58 +160,61 @@ void __f(const char* names, Arg1&& arg1, Args&&... args) {
     cout << " | ";
     __f(comma + 1, args...);
 }
-
-
+ 
+ 
 ////////////////////////////////////END TEMPLATE//////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-
-
-void solve()
-{
-    vll a(2);
-
-    read(a);
-
-    for(int i = 0; i < n; i++) {
-        
-    }
+void cal() {
 }
+
+
+void solve() {
+    
+    
+
+}
+
 
 int main()
 {
-
+ 
     #ifdef L
         local = 'L';
         time_t start, end;
         time(&start);
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
+        freopen("../../../input.txt", "r", stdin);
+        freopen("../../../output.txt", "w", stdout);
     #endif
-
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-
-        ll t;
-
-        t = 1;
-
-        // cin>>t;
-
-        while (t--)
-        {
-            solve();
-        }
-
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+ 
+    ll t1;
+    
+    t1 = 1;
+ 
+ 
+    cin>>t1;
+ 
+    cal();
+ 
+    while (t1--) {
+        solve();
+    }
+ 
+    
+ 
     #ifdef L
-        time(&end);
-        double time_taken = double(end - start);
-        cout << endl
-            << endl
-            << endl
-            << "Time taken by program is : " << fixed
-            << time_taken << setprecision(6);
-        cout << " sec " << endl;
+    time(&end);
+    double time_taken = double(end - start);
+    cout << endl
+        << endl
+        << endl
+        << "Time taken by program is : " << fixed
+        << time_taken << setprecision(6);
+    cout << " sec " << endl;
     #endif
 }
